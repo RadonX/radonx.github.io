375df785eaadcd32adf9ea2965e5452cd750e944
RadonX <radon93@hotmail.com>
2016-05-11 01:29:25 -0400

    modify text

--- js/shannon.js
+++ js/shannon.js
@@ -1,5 +1,5 @@
 var that;
-var text = "THEY ALSO HAVE BECOME UPSET WITH PERCEPTIONS THAT THE US WAS URGING ON THE MARKETS".toUpperCase();
+var text = "With Great Power Comes Great Responsibility".toUpperCase();
 var validChar = {"A":0,"B":0,"C":0,"D":0,"E":0,"F":0,"G":0,"H":0,"I":0,"J":0,"K":0,"L":0,"M":0,"N":0,"O":0,"P":0,"Q":0,"R":0,"S":0,"T":0,"U":0,"V":0,"W":0,"X":0,"Y":0,"Z":0,",":0,".":0," ":0};
 var charLen;
 var validCharStr = JSON.stringify(validChar);
